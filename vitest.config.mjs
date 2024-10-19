36b777a5d799f10f6d0377a23cdd861dc8b42173
Safwan Parkar <safwanparkar6@gmail.com>
2024-10-20 00:05:36 +0200

    chore: intial project setup

---
+++ vitest.config.mjs
@@ -0,0 +1,13 @@
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: [...configDefaults.exclude],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.mjs", "src/**/*.ts"],
+      exclude: [...configDefaults.coverage.exclude, "src/**/types.ts"],
+    },
+  },
+});
