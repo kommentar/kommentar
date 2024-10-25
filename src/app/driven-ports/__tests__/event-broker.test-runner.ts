c02c749263b3ec5f99bbc667cf4334ee0e26dfa6
Safwan Parker <safwan.parker@ingka.com>
2024-10-25 21:17:56 +0200

    feat: create port and in-memory adapter for event-broker

---
+++ src/app/driven-ports/__tests__/event-broker.test-runner.ts
@@ -0,0 +1,40 @@
+// event-broker.test.ts
+import { describe, expect, it, vi } from "vitest";
+import type { EventBroker, CloudEvent } from "../event-broker.js";
+
+const mockEvent: CloudEvent = {
+  specversion: "1.0",
+  type: "com.example.someevent",
+  source: "/mycontext",
+  subject: "123",
+  id: "A234-1234-1234",
+  time: "2020-08-24T14:15:22Z",
+  datacontenttype: "application/json",
+  data: { key: "value" },
+};
+
+const runEventBrokerTests = (eventBroker: EventBroker) => {
+  describe("EventBroker Port Tests", () => {
+    it("should publish an event", () => {
+      const publishSpy = vi.fn();
+      eventBroker.publish = publishSpy;
+
+      eventBroker.publish({ event: mockEvent });
+      expect(publishSpy).toHaveBeenCalledWith({ event: mockEvent });
+    });
+
+    it("should subscribe to an event and handle it", () => {
+      const handlerSpy = vi.fn();
+      eventBroker.subscribe({
+        type: "com.example.someevent",
+        handler: handlerSpy,
+      });
+
+      // Simulate an event being published
+      handlerSpy(mockEvent);
+      expect(handlerSpy).toHaveBeenCalledWith(mockEvent);
+    });
+  });
+};
+
+export { runEventBrokerTests };
---
+++ src/app/driven-ports/event-broker.ts
@@ -0,0 +1,27 @@
+type CloudEvent = {
+  specversion: string;
+  type: string;
+  source: string;
+  subject: string;
+  id: string;
+  time: string;
+  datacontenttype: string;
+  data: object;
+};
+
+type PublishEvent = ({ event }: { event: CloudEvent }) => void;
+
+type SubscribeToEvent = ({
+  type,
+  handler,
+}: {
+  type: string;
+  handler: (event: CloudEvent) => void;
+}) => void;
+
+type EventBroker = {
+  publish: PublishEvent;
+  subscribe: SubscribeToEvent;
+};
+
+export type { CloudEvent, EventBroker, PublishEvent, SubscribeToEvent };
