e3a65099e5a863b6f98a57ace4bde97960c0d244
Safwan Parker <safwan.parker@ingka.com>
2024-10-29 18:43:29 +0100

    refactor: change app parameters to object

--- src/app/domain/entities/app.ts
+++ src/app/domain/entities/app.ts
@@ -1,10 +1,22 @@
 import type { Comment } from "./comment.js";
 
 type App = {
-  getCommentsForHost: (hostId: string) => Promise<Comment[]>;
-  createCommentForHost: (hostId: string, content: string) => Promise<Comment>;
-  updateCommentById: (id: string, content: string) => Promise<Comment>;
-  deleteCommentById: (id: string) => Promise<void>;
+  getCommentsForHost: ({ hostId }: { hostId: string }) => Promise<Comment[]>;
+  createCommentForHost: ({
+    hostId,
+    content,
+  }: {
+    hostId: string;
+    content: string;
+  }) => Promise<Comment>;
+  updateCommentById: ({
+    id,
+    content,
+  }: {
+    id: string;
+    content: string;
+  }) => Promise<Comment>;
+  deleteCommentById: ({ id }: { id: string }) => Promise<void>;
 };
 
 export type { App };
