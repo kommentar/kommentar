afd020fc3308c84f2c278aa6bb266ae811d17674
Safwan Parker <safwan.parker@ingka.com>
2024-11-01 13:06:14 +0100

    feat: create policies for setting and invalidating cached comments

---
+++ src/app/policies/whenever-comment-updated-invalidate-cache.ts
@@ -0,0 +1,38 @@
+import type { Comment } from "../domain/entities/comment.js";
+import type { CacheStore } from "../driven-ports/cache-store.js";
+import type { EventBroker } from "../driven-ports/event-broker.js";
+
+type WheneverCommentUpdatedInvalidateCachePolicy = ({
+  eventBroker,
+  cacheStore,
+}: {
+  eventBroker: EventBroker;
+  cacheStore: CacheStore;
+}) => void;
+
+const wheneverCommentUpdatedInvalidateCache: WheneverCommentUpdatedInvalidateCachePolicy =
+  ({ eventBroker, cacheStore }) => {
+    eventBroker.subscribe({
+      type: "kommentar.comment.updated",
+      handler: (event) => {
+        const updatedComment = event.data as Comment;
+        const { hostId: key } = updatedComment;
+
+        const currentCachedComments = cacheStore.get(key) as
+          | Comment[]
+          | undefined;
+
+        if (!currentCachedComments) {
+          cacheStore.set(key, [updatedComment]);
+        } else {
+          const updatedCachedComments = currentCachedComments.map((comment) =>
+            comment.id === updatedComment.id ? updatedComment : comment,
+          );
+
+          cacheStore.set(key, updatedCachedComments);
+        }
+      },
+    });
+  };
+
+export { wheneverCommentUpdatedInvalidateCache };
